164702185f1884051d5dab600afa2fdc196d7964
Nagarehazh <proyectosexitososya@gmail.com>
2022-10-11 19:04:28 -0500

    Modal Front End Test implemented
    Modal styles render correctly
        √ Overlay renders correctly (60 ms)
        √ ModalContainer renders correctly (18 ms)
        √ EncabezadoModal renders correctly (11 ms)
        √ ButtonClose renders correctly (11 ms)

---
+++ client/src/components/Modal/ModalStyles.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+import {
+    Overlay,
+    ModalContainer,
+    EncabezadoModal,
+    ButtonClose,
+} from './ModalStyles'
+
+
+describe('Modal styles render correctly', () => {
+    it('Overlay renders correctly', () => {
+        const tree = renderer.create(<Overlay />).toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+    it('ModalContainer renders correctly', () => {
+        const tree = renderer.create(<ModalContainer />).toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+    it('EncabezadoModal renders correctly', () => {
+        const tree = renderer.create(<EncabezadoModal />).toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+    it('ButtonClose renders correctly', () => {
+        const tree = renderer.create(<ButtonClose />).toJSON()
+        expect(tree).toMatchSnapshot()
+    })
+})
+
